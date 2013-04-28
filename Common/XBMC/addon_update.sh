9e098a3337c2370030a68f779d8bf9f283d9e6ca
Nathan Dunn <nathan.dunn@UNSWalumni.com>
2013-04-28 21:58:29 +1000

    added XBMC config scripts placeholders

---
+++ Common/XBMC/addon_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#  raspi-build - Shell scripts for initializing Raspberry Pi 
+#  in a variety of configurations 
+# 
+#  (c) Copyright 2013  Nathan Dunn (nathan.dunn@UNSWalumni.com)
+# 
+#  raspi-build homepage: https://github.com/NathanSDunn/raspi-build
---
+++ Common/XBMC/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#  raspi-build - Shell scripts for initializing Raspberry Pi 
+#  in a variety of configurations 
+# 
+#  (c) Copyright 2013  Nathan Dunn (nathan.dunn@UNSWalumni.com)
+# 
+#  raspi-build homepage: https://github.com/NathanSDunn/raspi-build
+sudo chmod +x *.sh
+	
+# TODO create 5min cronjobs for device automount, wifi-reconnect (XBMC), XBMC addon updates
+# TODO install sickbeard/couch potato/transmission/addon-list xbmc addons
+# TODO configure sickbeard/couch potato/transmission
+# TODO setup media file locations/library cleaning/sickbeard & couch potato XBMC updater
