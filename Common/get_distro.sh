057977e489ab033cfaadaeec4b20a698744eb987
Nathan Dunn <nathan.dunn@UNSWalumni.com>
2013-04-28 22:07:08 +1000

    added install distro router placeholder

---
+++ Common/get_distro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#  raspi-build - Shell scripts for initializing Raspberry Pi 
+#  in a variety of configurations 
+# 
+#  (c) Copyright 2013  Nathan Dunn (nathan.dunn@UNSWalumni.com)
+# 
+#  raspi-build homepage: https://github.com/NathanSDunn/raspi-build
+
+#TODO find out what distribution we are running
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#  raspi-build - Shell scripts for initializing Raspberry Pi 
+#  in a variety of configurations 
+# 
+#  (c) Copyright 2013  Nathan Dunn (nathan.dunn@UNSWalumni.com)
+# 
+#  raspi-build homepage: https://github.com/NathanSDunn/raspi-build
+sudo chmod +x *.sh
+
+#TODO check distro version and route to the appropriate install.sh
+echo Not yet ready for use - please use the scripts in the appropriate directory for your distribution.
